a8123c9d8df4666fd6703a3ce11201424e708e0e
Lauri Mesilaakso <john.honeyvalley@gmail.com>
2016-11-23 03:11:15 +0100

    Edited the functions request, consctructURL
    Remark on the commit:
    The main.js and api.js are still in need of editing inorder that the
    functions work as expected.

--- scripts/api.js
+++ scripts/api.js
@@ -1,22 +1,47 @@
 // TODO Edit the code so that it functions properly (This is just copied from the tutorial)
 function APIConnect() {
-	var baseURL = '',
-		username = '',
-		password = '';
+    var baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php',
+        username = '',
+        password = '';
 
-	function constructURL(params) { }
+    function constructURL(params) {
+        var constructedURL = "?";
+        for (var key in params) {
+            if (params.hasOwnProperty(key)) {
+                console.log(key + " -> " + params[key]);
+                constructedURL += key + "=" + params[key] + "&";
+            }
+        }
+        constructedURL = constructedURL.replace('&','');
+        console.log(constructedURL);
+        return constructedURL;
+    }
 
-	function request(url, callback) { }
 
-	this.setUser = function(un, pw) {
-		username = un;
-		password = pw;
-	};
+    function request(url, callback) {
+        var xhr = new XMLHttpRequest(),
+            url = 'http://www.someurl.com?param1=value1&param2=value2';
 
-	this.fetchUsers = function(callback) {
-		var url = constructURL({action: 'user_get_all'});
-		request(url, callback);
-	};
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                console.log(this.responseText);
+            }
+        };
+        xhr.send();
+    }
 
-	this.fetchIOU = function(callback) { };
+    this.setUser = function(un, pw) {
+        username = un;
+        password = pw;
+    };
+
+    this.fetchUsers = function(callback) {
+        var url = constructURL({
+            action: 'user_get_all'
+        });
+        request(url, callback);
+    };
+
+    this.fetchIOU = function(callback) {};
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,49 @@
 // TODO Edit the code so that it functions properly (This is mainly just copied from the tutorial)
+function APIConnect() {
+    var baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php',
+        username = '',
+        password = '';
+
+    function constructURL(params) {
+        var constructedURL = baseURL + "?";
+        for (var key in params) {
+            if (params.hasOwnProperty(key)) {
+                console.log(key + " -> " + params[key]);
+                constructedURL += key + "=" + params[key] + "&";
+            }
+        }
+        constructedURL = constructedURL.slice(0, -1);
+        console.log(constructedURL);
+        return constructedURL;
+    }
+
+
+    function request(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                console.log(this.responseText);
+            }
+        };
+        xhr.send();
+    }
+
+    this.setUser = function(un, pw) {
+        username = un;
+        password = pw;
+    };
+
+    this.fetchUsers = function(callback) {
+        var url = constructURL({
+            action: 'user_get_all'
+        });
+        request(url, callback);
+    };
+
+    this.fetchIOU = function(callback) {};
+}
+
 function docLoaded(fn) {
 	if (document.readyState !== 'loading'){
 		fn();
@@ -6,12 +51,9 @@ function docLoaded(fn) {
 		document.addEventListener('DOMContentLoaded', fn);
 	}
 }
-function load() {
-  var api = new APIConnect(),
-  btn = document.getElementById("yourbuttonID");
 
-  api.setUser('jorass', 'jorass');
-  btn.addEventListener('click', function() { loadUsers(api) });
-
-  api.fetchIOU(function(usr) {  });
+function indexPageLoaded() {
+	var api = new APIConnect();
+	var testObject = {username: "Fred", password: "passwd", action: "be_a_badass!"};
+	api.constructURL(testObject);
 }
