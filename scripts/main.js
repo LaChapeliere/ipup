83a22f3a4e5fff8af04782a1f2e7b5414b3bb93c
Lauri Mesilaakso <john.honeyvalley@gmail.com>
2016-11-25 17:11:13 +0100

    Removed unnecessary functions and commented a jQuery call

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,3 @@
-
-// TODO Edit the code so that it functions properly (This is mainly just copied from the tutorial)
-/*
-function docLoaded(fn) {
-    if (document.readyState !== 'loading') {
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
-}
-*/
-function indexPageLoaded() {
-    var api = new APIConnect();
-    api.setUser('jorass','jorass');
-    loadUsers(api);
-}
-
-function loadUsers(params) {
-    params.fetchUsers(params);
-    // console.log(params);
-}
-
 /**
  * @author      Emma Barme
  * @version     0.2
