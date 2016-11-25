50d736ea97ef84ceed72062ff5c448df25903d60
Lauri Mesilaakso <john.honeyvalley@gmail.com>
2016-11-25 16:41:33 +0100

    Added jquery in html, further editing of main.js and moved code to api.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,39 +1,65 @@
-// TODO Edit the code so that it functions properly (This is just copied from the tutorial)
+// TODO Edit the code so that it functions properly
 function APIConnect() {
-    var baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php',
+    var baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php?',
         username = '',
         password = '';
+    /*    testObject = {
+            username: "Fred",
+            password: "passwd",
+            action: "be_a_badass!"
+        };
+        */
 
     function constructURL(params) {
-        var constructedURL = "?";
+        var constructedURL = baseURL +"username"+ "=" + username +"&" + "password"+"="+password+"&";
+    //    var userArray = [];
         for (var key in params) {
             if (params.hasOwnProperty(key)) {
-                console.log(key + " -> " + params[key]);
-                constructedURL += key + "=" + params[key] + "&";
+                constructedURL += key + "=" + params[key] +"&";
+            //    console.log(constructedURL);
+            //    userArray.push(key);
+            //    userArray.push(params[key]);
+            //    console.log(key + " -> " + params[key]);
+            //    console.log(userArray);
             }
         }
-        constructedURL = constructedURL.replace('&','');
-        console.log(constructedURL);
+    //    userArray = userArray.slice(4);
+    //    console.log(userArray);
+        constructedURL = constructedURL.slice(0, -1);
+        // console.log(constructedURL);
         return constructedURL;
     }
 
 
     function request(url, callback) {
-        var xhr = new XMLHttpRequest(),
-            url = 'http://www.someurl.com?param1=value1&param2=value2';
-
+        var xhr = new XMLHttpRequest();
         xhr.open('GET', url);
         xhr.onreadystatechange = function() {
-            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                console.log(this.responseText);
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log(xhr.responseText);
+        //        var info = JSON.parse(xhr.responseText);
+                callback(xhr.responseText);
             }
         };
         xhr.send();
     }
 
+// Using Jquery to access the server to get the JSON
+// This is an alternative to the request function
+    function requestWithJquery(url, callback) {
+        $.getJSON(url, callback);
+    }
+
+
     this.setUser = function(un, pw) {
         username = un;
         password = pw;
+/*
+        console.log(constructURL({
+            action: 'user_get_all',
+            action12: 'be_a_superman'
+        }));
+        */
     };
 
     this.fetchUsers = function(callback) {
@@ -43,5 +69,10 @@ function APIConnect() {
         request(url, callback);
     };
 
-    this.fetchIOU = function(callback) {};
+    this.fetchIOU = function(callback) {
+        var url = constructURL({
+            action: 'iou_get'
+        });
+        request(url, callback);
+    };
 }
