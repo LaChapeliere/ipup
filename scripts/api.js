76db8f7b97103ce208ad46cf669033b4a438790b
LaChapeliere <emmamarion.barme@orange.fr>
2016-11-25 16:24:31 +0100

    Wiring DB -fetchIOU and user_get_all
    Deleting APIConnect object definition from main
    Adding « using strict » strings
    Copying js from Emma’s api tutorial
    Adding some documentation
    Adding an testAPIConnection function to test the connection to the API
    Add basic console.log tests for fetchIOU and user_get_all

--- scripts/api.js
+++ scripts/api.js
@@ -1,47 +1,79 @@
-// TODO Edit the code so that it functions properly (This is just copied from the tutorial)
-function APIConnect() {
-    var baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php',
-        username = '',
-        password = '';
+/**
+ * @author      Emma Barme
+ * @version     0.2
+ */
 
+/**
+ * Constructor for the APIConnect object
+ */
+function APIConnect() {
+    'use strict';
+    var baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php', //The base url to connect to the database
+        username = '', //The field to hold the username
+        password = ''; // The field to hold the password
+    
+    /**
+    * Append the parameters for a request to the baseURL in order to create the url for the request
+    * @param params The dictionnary of the parameters
+    * @return The string of the request url
+    */
     function constructURL(params) {
-        var constructedURL = "?";
-        for (var key in params) {
-            if (params.hasOwnProperty(key)) {
-                console.log(key + " -> " + params[key]);
-                constructedURL += key + "=" + params[key] + "&";
+        var param,
+            url = baseURL;
+        url += "?";
+        for (param in params) {
+            if (params.hasOwnProperty(param)) {
+                url += param;
+                url += "=";
+                url += params[param];
+                url += "&";
             }
         }
-        constructedURL = constructedURL.replace('&','');
-        console.log(constructedURL);
-        return constructedURL;
+        return url.substr(0, url.length - 1);
     }
 
-
+    /**
+    * Send the request with a GET and call a function on the response
+    * @param url The url of the request
+    * @param callback The callback function to call on the response
+    */
     function request(url, callback) {
-        var xhr = new XMLHttpRequest(),
-            url = 'http://www.someurl.com?param1=value1&param2=value2';
-
+        var xhr = new XMLHttpRequest();
+        
         xhr.open('GET', url);
-        xhr.onreadystatechange = function() {
+        xhr.onreadystatechange = function () {
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                console.log(this.responseText);
+                callback(this.responseText);
             }
         };
         xhr.send();
     }
 
+    /**
+    * Method to set the username and password
+    * @param un The new username
+    * @param pw The new password
+    */
     this.setUser = function(un, pw) {
         username = un;
         password = pw;
     };
 
-    this.fetchUsers = function(callback) {
-        var url = constructURL({
-            action: 'user_get_all'
-        });
+    /**
+    * Method to fetch the users from the database and all their information
+    * @param callback The callback function to call on the response
+    */
+    this.fetchUsers = function (callback) {
+        var url = constructURL({action: 'user_get_all', username: username, password: password});
         request(url, callback);
     };
-
-    this.fetchIOU = function(callback) {};
+    
+    /**
+    * Method to fetch the basic information for the current user from the database
+    * @param callback The callback function to call on the response
+    */
+    this.fetchIOU = function (callback) {
+        var url = constructURL({action: 'iou_get', username: username, password: password});
+        request(url, callback);
+    };
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,59 +1,52 @@
-// TODO Edit the code so that it functions properly (This is mainly just copied from the tutorial)
-function APIConnect() {
-    var baseURL = 'http://pub.jamaica-inn.net/fpdb/api.php',
-        username = '',
-        password = '';
-
-    function constructURL(params) {
-        var constructedURL = baseURL + "?";
-        for (var key in params) {
-            if (params.hasOwnProperty(key)) {
-                console.log(key + " -> " + params[key]);
-                constructedURL += key + "=" + params[key] + "&";
-            }
-        }
-        constructedURL = constructedURL.slice(0, -1);
-        console.log(constructedURL);
-        return constructedURL;
-    }
+/**
+ * @author      Emma Barme
+ * @version     0.2
+ */
 
 
-    function request(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onreadystatechange = function() {
-            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                console.log(this.responseText);
-            }
-        };
-        xhr.send();
-    }
-
-    this.setUser = function(un, pw) {
-        username = un;
-        password = pw;
-    };
-
-    this.fetchUsers = function(callback) {
-        var url = constructURL({
-            action: 'user_get_all'
-        });
-        request(url, callback);
-    };
-
-    this.fetchIOU = function(callback) {};
+/**
+ * A test function executed when the index page is loaded, printing info in the console to test if the API is correctly wired
+ */
+function testAPIConnection() {
+    'use strict';
+    var api = new APIConnect();
+    
+    console.log("Test");
+    
+    //Test fetchIOU
+    api.setUser('jorass', 'jorass');
+    api.fetchIOU(function (usr) {
+        var info = JSON.parse(usr),
+            name = info.payload[0].first_name + " " + info.payload[0].last_name,
+            credit = info.payload[0].assets;
+        console.log(name);
+        console.log(credit);
+    });
+    api.setUser('edraug', 'edraug');
+    api.fetchIOU(function (usr) {
+        var info = JSON.parse(usr),
+            name = info.payload[0].first_name + " " + info.payload[0].last_name,
+            credit = info.payload[0].assets;
+        console.log(name);
+        console.log(credit);
+    });
+    
+    //Test user_get_all
+    api.setUser('jorass', 'jorass');
+    api.fetchUsers(function (usr) {
+        console.log(usr);
+    });
 }
 
+/**
+ * Utily function to execute js scripts linked to an html only when the page has finished loading
+ * @param The funtion to be executed
+ */
 function docLoaded(fn) {
+    'use strict';
 	if (document.readyState !== 'loading'){
 		fn();
 	} else {
 		document.addEventListener('DOMContentLoaded', fn);
 	}
-}
-
-function indexPageLoaded() {
-	var api = new APIConnect();
-	var testObject = {username: "Fred", password: "passwd", action: "be_a_badass!"};
-	constructURL(testObject);
 }
