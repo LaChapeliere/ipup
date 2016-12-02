f411e2e02ef2a388127598c23a28c4220cbf4770
Lauri Mesilaakso <john.honeyvalley@gmail.com>
2016-12-02 10:35:03 +0100

    Furthering of drag 'n drop

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ function docLoaded(fn) {
 function dragNDrop() {
     'use strict';
     var sidebar = document.getElementById("sidebar");
-    console.log(sidebar);
+
 
     /**
      * Function to change the id of the sidebar
@@ -66,46 +66,61 @@ function dragNDrop() {
             }
         });
     });
-    /*
-        $(function() {
 
-            // There's the gallery and the trash
-            var $contents = $("#beveragesTable"),
-                $shoppingCart = $("#sidebar");
 
-            // Let the gallery items be draggable
-            $("td", $contents).draggable({
-                cancel: "a.ui-icon", // clicking an icon won't initiate dragging
-                revert: "invalid", // when not dropped, the item will revert back to its initial position
-                containment: "document",
-                helper: "clone",
-                cursor: "move"
-            });
+    //var sum
+    // var totalPrice = $("#total")
+    // $("#total").html("The sum of purchases is" + sum)
+    $(function() {
 
-            // Let the trash be droppable, accepting the gallery items
-            $shoppingCart.droppable({
-                accept: "#beveragesTable > td",
-                /*    classes: {
-                       "ui-droppable-active": "ui-state-highlight"
-                   },*/
-    /*
-                drop: function(event, ui) {
-                    deleteImage(ui.draggable);
-                }
-            });
+        // There's the gallery and the trash
+        var $contents = $("#beveragesTable"),
+            $shoppingCart = $("#sidebar");
 
-            // Let the gallery be droppable as well, accepting items from the trash
-            /*    $contents.droppable({
-                    accept: "#trash li",
-                    classes: {
-                        "ui-droppable-active": "custom-state-active"
-                    },
-                    drop: function(event, ui) {
-                        recycleImage(ui.draggable);
-                    }
-                });*/
-    /*
+        // Let the gallery items be draggable
+        $("td", $contents).draggable({
+            cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+            revert: "invalid", // when not dropped, the item will revert back to its initial position
+            containment: "document",
+            helper: "clone",
+            cursor: "grab",
+            start: function(event, ui) {
+                //    changeSidebarId(sidebar);
+            },
+            stop: function(event, ui) {
+                //    changeSidebarId(sidebar);
+            }
 
+        });
+
+        // Let the trash be droppable, accepting the gallery items
+        $shoppingCart.droppable({
+            accept: ".product",
+            /*    classes: {
+                   "ui-droppable-active": "ui-state-highlight"
+               },*/
+
+            drop: function(event, ui) {
+                console.log(ui);
+                console.log(event);
+
+                //   deleteImage(ui.draggable);
+            }
+        });
+    });
+}
+// Let the gallery be droppable as well, accepting items from the trash
+/*    $contents.droppable({
+        accept: "#trash li",
+        classes: {
+            "ui-droppable-active": "custom-state-active"
+        },
+        drop: function(event, ui) {
+            recycleImage(ui.draggable);
+        }
+    });*/
+
+/*
             // Image deletion function
             var recycle_icon = "<a href='link/to/recycle/script/when/we/have/js/off' title='Recycle this image' class='ui-icon ui-icon-refresh'>Recycle image</a>";
 
@@ -128,27 +143,10 @@ function dragNDrop() {
                     });
                 });
             }
-
+*/
+/*
             // Image recycle function
-            var trash_icon = "<a href='link/to/trash/script/when/we/have/js/off' title='Delete this image' class='ui-icon ui-icon-trash'>Delete image</a>";
-
-            function recycleImage($item) {
-                $item.fadeOut(function() {
-                    $item
-                        .find("a.ui-icon-refresh")
-                        .remove()
-                        .end()
-                        .css("width", "96px")
-                        .append(trash_icon)
-                        .find("img")
-                        .css("height", "72px")
-                        .end()
-                        .appendTo($contents)
-                        .fadeIn();
-                });
-            }
-
-            // Image preview function, demonstrating the ui.dialog used as a modal window
+         // Image preview function, demonstrating the ui.dialog used as a modal window
             function viewLargerImage($link) {
                 var src = $link.attr("href"),
                     title = $link.siblings("img").attr("alt"),
@@ -168,7 +166,8 @@ function dragNDrop() {
                     }, 1);
                 }
             }
-
+*/
+/*
             // Resolve the icons behavior with event delegation
             $("ul.gallery > li").on("click", function(event) {
                 var $item = $(this),
@@ -185,96 +184,95 @@ function dragNDrop() {
                 return false;
             });
         });
-    */
+    
+*/
 
-
-    /**
-     * This method handles the drag 'n drop functionality
-     */
-    /*
-    $(function() {
-        $('#cartcontent').datagrid({
-            singleSelect: true
-        });
-        console.log(sidebar);
-        $('.product').draggable({
-            revert: true,
-
-            onStartDrag: function() {
-                console.log("Started dragging");
-                changeSidebarId(sidebar);
-                console.log(sidebar);
-
-                changeId(sidebar);
-                $(this).draggable('options').cursor = 'pointer';
-                //     $(this).draggable('proxy').css('z-index', 10);
-            },
-            onStopDrag: function() {
-                console.log("Stopped dragging");
-                console.log($(this));
-                changeSidebarId(sidebar);
-                console.log(sidebar);
-
-                $(this).draggable('options').cursor = 'pointer';
-            }
-        });
-        $('#sidebar').droppable({
-            onDragEnter: function(e, source) {
-                $(source).draggable('options').cursor = 'auto';
-                //     changeSidebarId(sidebar);
-                //     console.log($(this));
-
-            },
-            onDragLeave: function(e, source) {
-                console.log("Started onDragLeave");
-                //   changeSidebarId(sidebar);
-                $(source).draggable('options').cursor = 'not-allowed';
-            },
-            onDrop: function(e, source) {
-                console.log("Started onDrop");
-                var name = $(source).find('p:eq(0)').html();
-                //     console.log(source);
-                //     console.log(name);
-                var price = $(source).find('p:eq(1)').html();
-                //    console.log(price);
-                addProduct(name, parseFloat(price));
-            }
-        });
+/**
+ * This method handles the drag 'n drop functionality
+ */
+/*
+$(function() {
+    $('#cartcontent').datagrid({
+        singleSelect: true
     });
-    */
-    /**
-     * Function handles adding a product with a certain price to data array which holds information
-     * on the product and price and finally loads the new row or quantity number to datagrid.  
-     * @param name The name of the product
-     * @param price The price of the product placed 
-     */
-    /*
-    function addProduct(name, price) {
-        console.log("The addProduct price: " + price);
+    console.log(sidebar);
+    $('.product').draggable({
+        revert: true,
 
-        function add() {
-            console.log("Add method ran now.");
-            // For loop handles increasing the quantity of a same named product
-            for (var i = 0; i < data.total; i++) {
-                var row = data.rows[i];
-                if (row.name == name) {
-                    row.quantity += 1;
-                    return;
-                }
-            }
-            data.total += 1;
-            data.rows.push({
-                name: name,
-                quantity: 1,
-                price: price
-            });
+        onStartDrag: function() {
+            console.log("Started dragging");
+            changeSidebarId(sidebar);
+            console.log(sidebar);
+
+            changeId(sidebar);
+            $(this).draggable('options').cursor = 'pointer';
+            //     $(this).draggable('proxy').css('z-index', 10);
+        },
+        onStopDrag: function() {
+            console.log("Stopped dragging");
+            console.log($(this));
+            changeSidebarId(sidebar);
+            console.log(sidebar);
+
+            $(this).draggable('options').cursor = 'pointer';
         }
-        add();
-        totalCost += price;
-        console.log("The total cost is now: " + totalCost);
-        console.log(data);
-        $('#cartcontent').datagrid('loadData', data);
-        // Here is needed to maybe change the reference from #total to .total in the shopping cart
-        $('#total').html('Total: $' + totalCost);
-    }*/
-}
+    });
+    $('#sidebar').droppable({
+        onDragEnter: function(e, source) {
+            $(source).draggable('options').cursor = 'auto';
+            //     changeSidebarId(sidebar);
+            //     console.log($(this));
+
+        },
+        onDragLeave: function(e, source) {
+            console.log("Started onDragLeave");
+            //   changeSidebarId(sidebar);
+            $(source).draggable('options').cursor = 'not-allowed';
+        },
+        onDrop: function(e, source) {
+            console.log("Started onDrop");
+            var name = $(source).find('p:eq(0)').html();
+            //     console.log(source);
+            //     console.log(name);
+            var price = $(source).find('p:eq(1)').html();
+            //    console.log(price);
+            addProduct(name, parseFloat(price));
+        }
+    });
+});
+*/
+/**
+ * Function handles adding a product with a certain price to data array which holds information
+ * on the product and price and finally loads the new row or quantity number to datagrid.  
+ * @param name The name of the product
+ * @param price The price of the product placed 
+ */
+/*
+function addProduct(name, price) {
+    console.log("The addProduct price: " + price);
+
+    function add() {
+        console.log("Add method ran now.");
+        // For loop handles increasing the quantity of a same named product
+        for (var i = 0; i < data.total; i++) {
+            var row = data.rows[i];
+            if (row.name == name) {
+                row.quantity += 1;
+                return;
+            }
+        }
+        data.total += 1;
+        data.rows.push({
+            name: name,
+            quantity: 1,
+            price: price
+        });
+    }
+    add();
+    totalCost += price;
+    console.log("The total cost is now: " + totalCost);
+    console.log(data);
+    $('#cartcontent').datagrid('loadData', data);
+    // Here is needed to maybe change the reference from #total to .total in the shopping cart
+    $('#total').html('Total: $' + totalCost);
+}*/
