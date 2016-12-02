395289016b3e244d248944423bf8f7a8fd0ceb98
LaChapeliere <emmamarion.barme@orange.fr>
2016-12-02 16:44:44 +0100

    Creating functions that react to the click of the filtering buttons in consumer view
     + Correcting an index error in the display of drinks

--- scripts/beverages.js
+++ scripts/beverages.js
@@ -3,6 +3,11 @@
  * @version     0.1
  */
 
+/*
+ * The array holding the Slots representing the beverages in the consumer view.
+ */
+var beveragesSlots = [];
+
 /**
  * Constructor for the Slot object
  * Represents a slot in the machine, with the information necessary for its display, attached to it's corresponding html object
@@ -16,11 +21,20 @@ function Slot(beer_id, name, price, amount, category, alcohol, displayBlock) {
         amount = amount, //The amount of bottles of the beverages in the slot
         type = category, //The type of the beverage,
         alcohol = alcohol, //True if the drink contains alcohol
-        display = displayBlock; //The DOM element corresponding to the slot
+        display = displayBlock, //The DOM element corresponding to the slot
+        available = true; //Whether the drink is currently available or filtered out 
     
     /**
-    * Display the information for the beverage
-    */
+     * Getter of alcohol
+     * @return alcohol
+     */
+    this.isAlcoholic = function() {
+        return alcohol;
+    }
+    
+    /**
+     * Display the information for the beverage
+     */
     this.displayInfo = function() {
         var infoDisplay = display.getElementsByClassName("slotLeft")[0];
         infoDisplay.getElementsByClassName("drinkName")[0].textContent = name;
@@ -30,10 +44,28 @@ function Slot(beer_id, name, price, amount, category, alcohol, displayBlock) {
     }
     
     /**
-    * Fetch the image corresponding to the beverage category and set it as the display child image
-    */
+     * Fetch the image corresponding to the beverage category and set it as the display child image
+     */
     this.fetchImage = function() {
         display.getElementsByClassName("slotElement")[0].style.backgroundImage = 'url("./../resources/img/' + category + '.png")';
+    }
+    
+    /**
+     * Make the drink available is the consumer view by ungreying it and making it draggable
+     */
+    this.makeAvailable = function() {
+        
+        console.log("Make " + name + " available.")
+        available = true;
+    }
+    
+    /**
+     * Make the drink unavailable is the consumer view by greying it and making it non-draggable
+     */
+    this.makeUnavailable = function() {
+        
+        console.log("Make " + name + " unavailable.")
+        available = false;
     }
 }
 
@@ -49,25 +81,45 @@ function populateSlotsConsumer() {
         rows,
         cells,
         i = 0,
-        displayBlock;
+        displayBlock,
+        allBevButton = document.getElementById("allBevButton"),
+        alcoholOnlyButton = document.getElementById("alcoDrinksButton"),
+        softOnlyButton = document.getElementById("softDrinksButton");
     
-    console.log(beveragesTable);
-    
+    //Fetch the content and create a Slot object to hold it
     api.fetchContent( function(machineContent) {
         var beverage,
-            beverageInfo;
+            beverageInfo,
+            index;
         
         //For each slot in the machine
         rows = beveragesTable.getElementsByTagName("tr");
         for (i = 0; i < rows.length; i++) {
             cells = rows[i].getElementsByTagName("td");
             for (j = 0; j < cells.length; j++) {
-                beverageInfo = machineContent[i * rows.length + j];
+                index = i * cells.length + j;
+                beverageInfo = machineContent[index];
                 beverage = new Slot(beverageInfo.beer_id, beverageInfo.name, beverageInfo.price, beverageInfo.amount, beverageInfo.category, beverageInfo.alcoholic, cells[j]);
+                //Send the information to the display
                 beverage.displayInfo();
+                beveragesSlots.push(beverage);
             }
         }
     });
+        
+    //Wire the filter buttons
+    allBevButton.onclick = function () {
+        console.log("All");
+        filterAlcoholDrinks(true, true);
+    };
+    softOnlyButton.onclick = function() {
+        console.log("Soft only");
+        filterAlcoholDrinks(false, true);
+    };
+    alcoholOnlyButton.onclick = function() {
+        console.log("Alco only");
+        filterAlcoholDrinks(true, false);
+    };
 }
 
 /**
@@ -94,4 +146,38 @@ function beverageCategory(providedInfo) {
     }
     
     return [category, alcoholic];
+}
+
+/**
+ * Manages the filtering out of drinks in the consumer view
+ * @param displayAlco True if the alcoholic drinks should be available
+ * @param displaySoft True if the non-acoholic drinks should be available
+ */
+function filterAlcoholDrinks(displayAlco, displaySoft) {
+    'use strict';
+    var i = 0,
+        slot;
+    //For each beverage
+    for (; i < beveragesSlots.length; i++) {
+        slot = beveragesSlots[i];
+        //If the beverage is alcoholic
+        if (slot.isAlcoholic()) {
+            if (displayAlco) {
+                slot.makeAvailable();
+            }
+            else {
+                slot.makeUnavailable();
+            }
+            continue;
+        }
+        
+        
+        //If the beverage is non-acoholic
+        if (displaySoft) {
+            slot.makeAvailable();
+        }
+        else {
+            slot.makeUnavailable();
+        }
+    }
 }
