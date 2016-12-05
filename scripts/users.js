b1aee1aacfc9cdfd9f2dd77fc145bfdf04c36084
LaChapeliere <emmamarion.barme@orange.fr>
2016-12-05 16:20:19 +0100

    Add addUser and editUser functions

--- scripts/users.js
+++ scripts/users.js
@@ -37,3 +37,50 @@ function loadConsumerPage(api, usn, pwd, info) {
     //Load the main consumer page
     window.location.assign("../html/index.html");
 }
+
+/**
+ * Add a user to the database
+ * @param api The api connection to the database. The connected user must be an admin.
+ * @param newUsername The username of the new user
+ * @param newPassword The password of the new user
+ * @param firstName The new user first name
+ * @param lastName The new user last name in the database
+ * @param email The new user's email
+ * @param phone The new user's phone
+ * @TODO Add safeguard if the connected user is not an admin.
+ * @return An error string, empty if the function completed without error
+ */
+function addUser(api, newUsername, newPassword, firstName, lastName, email, phone) {
+    apiAnswer = api.editUser(newUsername, newPassword, firstName, lastName, email, phone, function (answer) {
+        return answer;
+    })
+    
+    if (apiAnswer.type == "error") {
+        return apiAnswer.payload[0].msg;
+    }
+    return "";
+}
+
+/**
+ * Edit a user info in the database
+ * @param api The api connection to the database. The connected user must be an admin.
+ * @param targetUsername The username of the user to modify
+ * @param newPassword The new password of the user
+ * @param firstName The new user first name
+ * @param lastName The new user last name in the database
+ * @param email The new user's email
+ * @param phone The new user's phone
+ * @TODO Add safeguard if the connected user is not an admin.
+ * @TODO Add safeguard is user to be modified do no exist
+ * @return An error string, empty if the function completed without error
+ */
+function addUser(api, targetUsername, newPassword, firstName, lastName, email, phone) {
+    apiAnswer = api.editUser(targetUsername, newPassword, firstName, lastName, email, phone, function (answer) {
+        return answer;
+    })
+    
+    if (apiAnswer.type == "error") {
+        return apiAnswer.payload[0].msg;
+    }
+    return "";
+}
