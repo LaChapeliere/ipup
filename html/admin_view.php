5e80533a292f501cff433f200ab42ac4618fadfb
MSkogsmo <markus.skogsmo.93@gmail.com>
2016-12-06 11:24:08 +0100

    Adding something

--- html/admin_view.php
+++ html/admin_view.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8" />
 <link href="../css/reset.css" rel="stylesheet" />
 <link href="../css/style.css" rel="stylesheet" />
+<link href="../css/admin.css" rel="stylesheet" /> <!--Overriding-->
 <!--<script src="http://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 -->
 <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
