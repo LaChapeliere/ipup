1d6f27b3f6044e9c6881bee194e20ab39933c72c
mikelebike <mikelebike@student250-83.eduroam.uu.se>
2016-12-06 15:54:31 +0100

    worked on admin view
    1) cleaned up and started working in admin.css
    
    2) created table and made it sortable
    
    3) added a SortTable.js script.
    
    unfortunately the js had to be included in admin_view.php and i
    couldn’t separate them. Maybe someone else can do it.

--- html/admin_view.php
+++ html/admin_view.php
@@ -24,6 +24,7 @@
     docLoaded(populateSlotsConsumer);
 </script>
 
+
 <!-- Wrapper for all content-->
 
 <div id="wrapper">
@@ -31,6 +32,7 @@
 
         <!-- The top bar starts here-->
         <header id="topBar">
+            
             <dl>
                 <dt><button>Users</button></dt>
                 <dt><button>Machine stock</button></dt>
@@ -71,60 +73,353 @@
         <!-- THE MAIN CONTENT STARTS HERE, I.E NOT TOP BAR OR SIDE BAR -->
 
         <!-- A table for all of the drinks to appear in-->
-            <table id="stockTable" style="width:100%">
+            
+        
+        <table id="stockTable" style="width:100%">
+        <thead>
                 <tr class ="stock_admin">
-                    <th class = "head_admin">Name</th>
-                    <th class = "head_admin">Price</th>
-                    <th class = "head_admin">Count</th>
-                    <th class = "head_admin">Category</th>
+                    <th class = "head_admin"> <a href="javascript:SortTable(0,'T');">Le Name</a></th>
+                    <th class = "head_admin"> <a href="javascript:SortTable(0,'T');">Le Price</a></th>
+                    <th class = "head_admin"> <a href="javascript:SortTable(0,'T');">Le Count</a></th>
+                    <th class = "head_admin"> <a href="javascript:SortTable(0,'T');">Le Category</a></th>
+                    
+                </tr>
+        </thead>
+        <tbody>      
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Fame</td>
+                            <td class="price_entry">Rice here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
                 </tr>
                 
                 <tr class ="stock_admin">
-                    <div class="slotLeft">
-                    <td class="drinkName">Name</td>
-                            <td class="price">Price here</td>
-                            <td class="stock">Stock here</td>
-                            <td class="category">Category here</td>
-                    </div>
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
                 </tr>
                 
                 <tr class ="stock_admin">
-                    <div class="slotLeft">
-                    <td class="drinkName">Name</td>
-                            <td class="price">Price here</td>
-                            <td class="stock">Stock here</td>
-                            <td class="category">Category here</td>
-                    </div>
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
                 </tr>
                 
                 <tr class ="stock_admin">
-                    <div class="slotLeft">
-                    <td class="drinkName">Name</td>
-                            <td class="price">Price here</td>
-                            <td class="stock">Stock here</td>
-                            <td class="category">Category here</td>
-                    </div>
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
                 </tr>
                 
                 <tr class ="stock_admin">
-                    <div class="slotLeft">
-                    <td class="drinkName">Name</td>
-                            <td class="price">Price here</td>
-                            <td class="stock">Stock here</td>
-                            <td class="category">Category here</td>
-                    </div>
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">88839</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">StOIHSAIUDHIre</td>
+                            <td class="category_entry">Category here</td>
+                   
                 </tr>
                 
                 
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">blarrghhhharjj</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+            <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">FATGEMONY</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+            <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+            </tbody>      
             </table>
-
+        </div>
 
         </div>
-    </div>
 
 
 
 <body>
+    
+    
+/*
+    The following is a script taken from http://www.willmaster.com/library/features/sorting-a-table-with-javascript.php
+    
+    */
+    
+    <script type="text/javascript">
+/* 
+   Willmaster Table Sort
+   Version 1.1
+   August 17, 2016
+   Updated GetDateSortingKey() to correctly sort two-digit months and days numbers with leading 0.
+   Version 1.0, July 3, 2011
+
+   Will Bontrager
+   http://www.willmaster.com/
+   Copyright 2011,2016 Will Bontrager Software, LLC
+
+   This software is provided "AS IS," without 
+   any warranty of any kind, without even any 
+   implied warranty such as merchantability 
+   or fitness for a particular purpose.
+   Will Bontrager Software, LLC grants 
+   you a royalty free license to use or 
+   modify this software provided this 
+   notice appears on all copies. 
+*/
+//
+// One placed to customize - The id value of the table tag.
+
+var TableIDvalue = "stockTable";
+
+//
+//////////////////////////////////////
+var TableLastSortedColumn = -1;
+function SortTable() {
+var sortColumn = parseInt(arguments[0]);
+var type = arguments.length > 1 ? arguments[1] : 'T';
+var dateformat = arguments.length > 2 ? arguments[2] : '';
+var table = document.getElementById(TableIDvalue);
+var tbody = table.getElementsByTagName("tbody")[0];
+var rows = tbody.getElementsByTagName("tr");
+var arrayOfRows = new Array();
+type = type.toUpperCase();
+dateformat = dateformat.toLowerCase();
+for(var i=0, len=rows.length; i<len; i++) {
+	arrayOfRows[i] = new Object;
+	arrayOfRows[i].oldIndex = i;
+	var celltext = rows[i].getElementsByTagName("td")[sortColumn].innerHTML.replace(/<[^>]*>/g,"");
+	if( type=='D' ) { arrayOfRows[i].value = GetDateSortingKey(dateformat,celltext); }
+	else {
+		var re = type=="N" ? /[^\.\-\+\d]/g : /[^a-zA-Z0-9]/g;
+		arrayOfRows[i].value = celltext.replace(re,"").substr(0,25).toLowerCase();
+		}
+	}
+if (sortColumn == TableLastSortedColumn) { arrayOfRows.reverse(); }
+else {
+	TableLastSortedColumn = sortColumn;
+	switch(type) {
+		case "N" : arrayOfRows.sort(CompareRowOfNumbers); break;
+		case "D" : arrayOfRows.sort(CompareRowOfNumbers); break;
+		default  : arrayOfRows.sort(CompareRowOfText);
+		}
+	}
+var newTableBody = document.createElement("tbody");
+for(var i=0, len=arrayOfRows.length; i<len; i++) {
+	newTableBody.appendChild(rows[arrayOfRows[i].oldIndex].cloneNode(true));
+	}
+table.replaceChild(newTableBody,tbody);
+} // function SortTable()
+
+function CompareRowOfText(a,b) {
+var aval = a.value;
+var bval = b.value;
+return( aval == bval ? 0 : (aval > bval ? 1 : -1) );
+} // function CompareRowOfText()
+
+function CompareRowOfNumbers(a,b) {
+var aval = /\d/.test(a.value) ? parseFloat(a.value) : 0;
+var bval = /\d/.test(b.value) ? parseFloat(b.value) : 0;
+return( aval == bval ? 0 : (aval > bval ? 1 : -1) );
+} // function CompareRowOfNumbers()
+
+function GetDateSortingKey(format,text) {
+if( format.length < 1 ) { return ""; }
+format = format.toLowerCase();
+text = text.toLowerCase();
+text = text.replace(/^[^a-z0-9]*/,"");
+text = text.replace(/[^a-z0-9]*$/,"");
+if( text.length < 1 ) { return ""; }
+text = text.replace(/[^a-z0-9]+/g,",");
+var date = text.split(",");
+if( date.length < 3 ) { return ""; }
+var d=0, m=0, y=0;
+for( var i=0; i<3; i++ ) {
+	var ts = format.substr(i,1);
+	if( ts == "d" ) { d = date[i]; }
+	else if( ts == "m" ) { m = date[i]; }
+	else if( ts == "y" ) { y = date[i]; }
+	}
+d = d.replace(/^0/,"");
+if( d < 10 ) { d = "0" + d; }
+if( /[a-z]/.test(m) ) {
+	m = m.substr(0,3);
+	switch(m) {
+		case "jan" : m = String(1); break;
+		case "feb" : m = String(2); break;
+		case "mar" : m = String(3); break;
+		case "apr" : m = String(4); break;
+		case "may" : m = String(5); break;
+		case "jun" : m = String(6); break;
+		case "jul" : m = String(7); break;
+		case "aug" : m = String(8); break;
+		case "sep" : m = String(9); break;
+		case "oct" : m = String(10); break;
+		case "nov" : m = String(11); break;
+		case "dec" : m = String(12); break;
+		default    : m = String(0);
+		}
+	}
+m = m.replace(/^0/,"");
+if( m < 10 ) { m = "0" + m; }
+y = parseInt(y);
+if( y < 100 ) { y = parseInt(y) + 2000; }
+return "" + String(y) + "" + String(m) + "" + String(d) + "";
+} // function GetDateSortingKey()
+</script>
+
 
 </body>
 
