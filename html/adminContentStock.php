a137f9ff297e94e2b10b983e858bc7f662a84f04
mikelebike <mikelebike@Michaels-MacBook-Air.local>
2016-12-08 10:51:53 +0100

    admin work

---
+++ html/adminContentStock.php
@@ -0,0 +1,223 @@
+<div id="adminContent">
+
+            
+            
+            
+            
+            
+     
+            
+        <div id="tableDiv">    
+        <table id="stockTable">
+        <thead>
+                <tr class ="stock_admin">
+                    <th class = "head_admin"> <a href="javascript:SortTable(0,'T');">Le Name</a></th>
+                    <th class = "head_admin"> <a href="javascript:SortTable(1,'T');">Le Price</a></th>
+                    <th class = "head_admin"> <a href="javascript:SortTable(2,'T');">Le Count</a></th>
+                    <th class = "head_admin"> <a href="javascript:SortTable(3,'T');">Le Category</a></th>
+                    
+                </tr>
+        </thead>
+            
+        <tbody id="adminTableBody">      
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Fame</td>
+                            <td class="price_entry">Rice here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">So expensive</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">It's over 9000!</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">blarrghhhharjj</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+            <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Unicorn Blood</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+            <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">Name</td>
+                            <td class="price_entry">Price here</td>
+                            <td class="stock_entry">Stock here</td>
+                            <td class="category_entry">Category here</td>
+                   
+                </tr>
+                
+            </tbody>      
+            </table>
+            </div>
+
+        </div>
