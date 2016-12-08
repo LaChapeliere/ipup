49170919963b820972cd3414191a4bb764363838
MSkogsmo <markus.skogsmo.93@gmail.com>
2016-12-08 14:14:58 +0100

    Making history stats

---
+++ html/history_stats_content.php
@@ -0,0 +1,109 @@
+<!-- History Stats Content -->
+
+ <tr>
+     <h1>Stats</h1>
+     <div id="stats">
+            <table id="beveragesTable">
+            <thead>
+                <tr class ="stock_admin">
+                    <th>My Favorite</th>
+                    <th>Company Favorite</th>
+                    <th>Most Previous Purschase</th>
+                    <th>Mikelebike's Butt</th>   
+                </tr>
+            </thead>
+                <!-- Row one -->
+                <tr>
+                     <td>
+                        <div class="product">
+                        <div class="slotElement"> </div>
+                        <div class="slotLeft">
+                            <p class="drinkName">Name</p>
+                            <p class="price">Price here</p>
+                            <p class="stock">Stock here</p>
+                            <p class ="imgDescription">Double-click or drag to add to cart</p>
+                        </div>
+                        </div>
+                    </td>
+                     <td>
+                        <div class="product">
+                        <div class="slotElement"> </div>
+                        <div class="slotLeft">
+                            <p class="drinkName">Name</p>
+                            <p class="price">Price here</p>
+                            <p class="stock">Stock here</p>
+                            <p class ="imgDescription">Double-click or drag to add to cart</p>
+                        </div>
+                        </div>
+                    </td>
+                     <td>
+                        <div class="product">
+                        <div class="slotElement"> </div>
+                        <div class="slotLeft">
+                            <p class="drinkName">Name</p>
+                            <p class="price">Price here</p>
+                            <p class="stock">Stock here</p>
+                            <p class ="imgDescription">Double-click or drag to add to cart</p>
+                        </div>
+                        </div>
+                    </td>
+                     <td>
+                        <div class="product">
+                        <div class="slotElement"> </div>
+                        <div class="slotLeft">
+                            <p class="drinkName">Name</p>
+                            <p class="price">Price here</p>
+                            <p class="stock">Stock here</p>
+                            <p class ="imgDescription">Double-click or drag to add to cart</p>
+                        </div>
+                        </div>
+                    </td>
+                </tr>
+         </table>
+     </div>
+    
+     <h1>History</h1>
+     
+     <div id="history">
+            <table id="beveragesTable">
+      <tr class ="stock_admin">
+                    
+                            <td class="name_entry">What?</td>
+                            <td class="price_entry">When?</td>
+                            <td class="stock_entry">How Much?</td>
+                            <td class="category_entry">Current Balance</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">What?</td>
+                            <td class="price_entry">When?</td>
+                            <td class="stock_entry">How Much?</td>
+                            <td class="category_entry">Current Balance</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">What?</td>
+                            <td class="price_entry">When?</td>
+                            <td class="stock_entry">How Much?</td>
+                            <td class="category_entry">Current Balance</td>
+                   
+                </tr>
+                
+                <tr class ="stock_admin">
+                    
+                            <td class="name_entry">What?</td>
+                            <td class="price_entry">When?</td>
+                            <td class="stock_entry">How Much?</td>
+                            <td class="category_entry">Current Balance</td>
+                   
+                </tr>
+         </table>
+     </div>
+</tr>
+
+                
+    
--- html/historystats.php
+++ html/historystats.php
@@ -7,27 +7,33 @@
 	<link href="../css/wrapper.css" rel="stylesheet" />
 	<link href="../css/topBar.css" rel="stylesheet" />
 	<link href="../css/sideBar.css" rel="stylesheet" />
+    <link href="../css/historystats.css" rel="stylesheet" />
 	<!-- <script src="../scripts/api.js"></script> -->
 	<script src="../scripts/main.js"></script>
+    
 <div id="wrapper">
+   
 	<div class="container">
-        
+         
         <?php include "statsTopbar.php";?>
         <?php include "sideBar.php";?>
+        <?php include "history_stats_content.php";?>
 		
+        
 
          <!-- THE MAIN CONTENT STARTS HERE, I.E NOT TOP BAR OR SIDE BAR -->
 
             
-   			<div id="historycontent">
-   				What do we want here? :) 
-   			</div>	
-
-	
-	
-
-<body>
-
-</body>
-
+    </div>
+    </div>
+    
 </html>
+               
+
+
+
+
+    
+
+
+    
