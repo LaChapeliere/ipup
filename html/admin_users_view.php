c0993768b2941f447407799129ebbb1d5fa152fb
mikelebike <mikelebike@Michaels-MacBook-Air.local>
2016-12-09 16:38:07 +0100

    added adminSideBar

--- html/admin_users_view.php
+++ html/admin_users_view.php
@@ -47,7 +47,7 @@
 <div id="wrapper">
     <div class="container">
         <?php include "topBar.php";?>
-        <?php include "sideBar.php";?>
+        <?php include "adminSideBar.php";?>
         <?php include "adminContentUsers.php";?>
  
 </div>
