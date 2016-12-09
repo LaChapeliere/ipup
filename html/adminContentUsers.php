6d77fa793f54b9ab5ec6b875547848cd1261decc
mikelebike <mikelebike@Michaels-MacBook-Air.local>
2016-12-09 14:19:57 +0100

    added admin user edit

---
+++ html/adminContentUsers.php
@@ -0,0 +1,118 @@
+<div id="adminContent">
+
+<div id="buttonToolbar">            
+    <div id="addUser">
+        <button id="addUserButton">Add New User</button>   
+    </div>
+                    
+
+    <input id="searchBar" type="search" class="light-table-filter" data-table="stock-table" placeholder="Type to search">
+</div>
+             
+           
+            
+        
+<table id="stockTableHead">
+    <thead>
+        <tr class ="stock_admin">
+            <th class = "head_admin"> <a href="javascript:SortTable(0,'T');">Le Name</a></th>
+            <th class = "head_admin"> <a href="javascript:SortTable(1,'N');">Debt</a></th>
+            <th class = "head_admin"> <a href="javascript:SortTable(2,'T');">User ID</a></th>
+            <th class = "head_admin"> <a href="javascript:SortTable(2,'T');">Is admin</a></th>
+                    
+        </tr>
+    </thead>
+</table>
+    
+    
+    
+<div id="tableDiv">
+    <table id="userTable" class="user-table table">    
+        <tbody id="adminUserTableBody">           
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Mike</td>
+                <td class="user_karma">The universe loves me</td>
+                <td class="user_debt">700</td>
+                <td class="user_popularity">mr douche</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            <tr class ="stock_admin">
+                <td class="user_name">Markus</td>
+                <td class="user_karma">Really bad. I mean so, so bad!!</td>
+                <td class="user_debt">-9000</td>
+                <td class="user_popularity">mr popular</td>
+            </tr>
+            
+        </tbody>      
+    </table>
+</div>
+
+
+</div>
+
+
