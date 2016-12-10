67105cdde1c94db31ca7dc33875d772443c650c9
MSkogsmo <markus.skogsmo.93@gmail.com>
2016-12-10 12:41:58 +0100

    Admin User Pop Up

--- html/popup_user.php
+++ html/popup_user.php
@@ -4,49 +4,37 @@
               
                     <div class="box">
                         
-                        <a class="button" href=#popupStockEdit>Click me!</a>
+                        <a class="button" href=#popupUserEdit>Click me!</a>
                     </div>
                 
 
 
 
 
- <div id="popupStockEdit" class="overlay">
+ <div id="popupUserEdit" class="overlay">
 	<div class="popup">
         <form>
-		<h2>Edit Beverage Info</h2>
+		<h2>Edit User Info</h2>
 		<div class="popupContent">
             
             <button type="button" id="closeCross" onclick="closeEditPopup()">&times;</button>
-            <!-- Input fields for the beverages -->
             
-            Beverage name:<br>
-            <div class="inputDiv"><input type="text" id="bevName" placeholder="Beverage Name"></div><br>
-            Quantity in stock:<br>
-            <div class="inputDiv"><input type="number" id="bevQuantity" placeholder="Quantity"></div><br>
-            Price of beverage<br>
-            <div class="inputDiv"><input type="text" id="bevPrice" placeholder="Price"></div><br>
+            <!-- Input fields for the user -->
             
-            <div class="inputDiv"><input type="text" id="bevId" placeholder="Id" hidden></div><br>
+            <h3>User Name</h3>
+            <div class="inputDiv"><input type="text" id="userName" placeholder="Name"></div>
+            <h3>Balance</h3>
+            <div class="inputDiv"><input type="number" id="userBalance" placeholder="Amount"></div>
+            <h3>User ID</h3>
+            <div class="inputDiv"><input type="text" id="userID" placeholder="ID"></div>
+            <h3>Password</h3>
+            <div class="inputDiv"><input type="password" id="userPassword" placeholder="Password"></div>
             
             <!-- Beverage typ selection meny-->
-            Beverage type:
-            <div>
-            <select id="bevType" name="bvt">
-                <option disabled selected value> -- select a beverage -- </option>
-                <option>Soft Drink</option>
-                <option>Lager</option>
-                <option>Stout</option>
-                <option>Ale</option>
-                <option>Beer</option>
-                <option>White Wine</option>
-                <option>Red Wine</option>
-                <option>Cider</option>
-            </select>
-            </div>
             
-            <!-- Toggle button for alcohol/non alcohol -->
-            Alcoholic?<br>
+            <!-- Toggle button for Admin or NOT -->
+            
+            <h3>Admin Powers</h3>
             <div id="toggle_div">
                 <label class="switch">
                <input type="checkbox"> 
@@ -59,7 +47,6 @@
             
             <div>
                 <button type="button" id="save" onclick="saveEditBev()">Save</button>
-                <input type="reset">
                 <button type="button" id="cancel" onclick="closeEditPopup()">Cancel</button>
             </div>
 		</div>
