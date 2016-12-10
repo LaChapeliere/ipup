467577a273967f48c47d543b9131fd2906c9756e
mikelebike <mikelebike@Michaels-MacBook-Air.local>
2016-12-10 17:37:29 +0100

    remove hist/stat from admin view

--- html/adminTopBar.php
+++ html/adminTopBar.php
@@ -4,10 +4,6 @@
         <dt><button id="stockButton" onclick="linkFormSubmit('admin_stock.php');">Stock</button></dt>
             <dt><button id="usersButton" onclick="linkFormSubmit('admin_users_view.php');">Users</button></dt>
             <dt><button id="machineContentButton" onclick="linkFormSubmit('admin_machine.php');">Machine Content</button></dt>
-            <dt>
-                <a id="historyStatsButton" type="button" href=#customerContentPopup >History/Stats</a>
-            </dt>
-
     </dl>
 </header>
 
