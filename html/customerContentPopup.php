34c37bfecfbdfd1e07dbd9368aba6aabe90d2c2e
MSkogsmo <markus.skogsmo.93@gmail.com>
2016-12-29 18:29:34 +0100

    Making a log in error and some improvements

--- html/customerContentPopup.php
+++ html/customerContentPopup.php
@@ -7,6 +7,7 @@
 		<h2>Sure you want to continue?</h2>
         <h3>Your cart is not empty! Leaving this page will unfortunately empty the cart.</h3>
 		<a class="close" id="closeCross" href="#">&times;</a>
+        <br>
         <div id="buttonDiv">
             <a class="close" href="#"><button id="no"> Cancel</button></a>
             <button id="yes" onclick="linkFormSubmit('historystats.php');">Continue</button>
