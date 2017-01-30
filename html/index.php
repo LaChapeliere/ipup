64ab82ee2d942b56dda84d823284775419e4d547
LaChapeliere <emmamarion.barme@orange.fr>
2017-01-30 10:59:27 +0100

    Responsive login page
    Image size adapts to height instead of width to avoid pushing the other
    content out of the page
    Form fields align horizontally instead of vertically if not enough room

--- html/index.php
+++ html/index.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-    <img src="../resources/img/logo.gif" alt="Jamaica Inn" style="width:45%;">
+    <img src="../resources/img/logo.gif" alt="Jamaica Inn" style="height:45%;">
     <section class="container">
         <div class="login">
             <h1>Log in by filling in the fields below</h1>
